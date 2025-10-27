3ace4e240ff2385e3929ff0fe7eadeb0aacefa55
Chandrakant6 <chandrakant2005turkar@gmail.com>
2025-10-27 14:48:51 +0530

    cleaned postgres 2

--- load_to_potgres.py
+++ load_to_potgres.py
@@ -1,15 +1,15 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-# ---------- Step 1: Read the merged CSV ----------
+# Read the merged CSV 
 merged_df = pd.read_csv("data/merged_output.csv")
 
-# ---------- Step 2: (Optional minimal cleaning) ----------
+# minimal cleaning
 merged_df.drop_duplicates(inplace=True)
 merged_df = merged_df.fillna(value=pd.NA)
 
-# ---------- Step 3: Connect to PostgreSQL ----------
-# Example credentials — change these to your local setup
+# Connect to PostgreSQL 
+# Example credentials —
 DB_USER = "postgres"
 DB_PASSWORD = "postgres"
 DB_HOST = "localhost"
@@ -18,7 +18,7 @@ DB_NAME = "data_engg"
 
 engine = create_engine(f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}")
 
-# ---------- Step 4: Load the data ----------
+# Load the data 
 TABLE_NAME = "merged_data"
 
 merged_df.to_sql(TABLE_NAME, engine, if_exists="replace", index=False)
